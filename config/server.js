9b987f95a30943052c4e6f0a84c6ed284dd755e2
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2022-08-15 13:48:14 +0545

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bbe6ef2d4610c2c783975bdd926ea90b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cfb3072f-88fc-4dd7-b46c-66e631524759'
+};
